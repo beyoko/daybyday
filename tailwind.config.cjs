71a27f86d468909cbdf8e10ded3434076cd23435
beyoko <blogbetest@protonmail.com>
2023-05-04 21:16:22 +1000

    test

---
+++ tailwind.config.cjs
@@ -0,0 +1,12 @@
+module.exports = {
+  content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
+  darkMode: 'class',
+  theme: {
+    extends: {
+      fontFamily: {
+        mplus: ['Roboto', 'helvetica', 'arial', 'sans-serif']
+      }
+    }
+  },
+  plugin: []
+}
