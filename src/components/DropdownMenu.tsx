6ce0b45c380eb4ee7e6c64539fa2ac7c28d8e877
beyoko <blogbetest@protonmail.com>
2023-05-21 16:59:49 +1000

    day

--- src/components/DropdownMenu.tsx
+++ src/components/DropdownMenu.tsx
@@ -15,7 +15,7 @@ export default function DropdownMenu({ tags }: Props) {
     >
       <div>
         <Menu.Button
-          className="inline-flex justify-center rounded-md border border-gray-400 dark:border-zinc-700 px-2 py-2 text-sm font-medium shadow-sm hover:bg-gray-950 hover:text-gray-50 hover:dark:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2 focus:ring-offset-gray-100 transition-all"
+          className="inline-flex justify-center rounded-md border border-gray-400 dark:border-zinc-700 px-2 py-2 text-sm font-medium shadow-sm hover:bg-gray-950 hover:text-gray-50 hover:dark:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-gray-500 focus:ring-offset-2 focus:ring-offset-gray-100 transition-all"
           aria-label="menu"
         >
           <IoMenu className="h-5 w-5" />
