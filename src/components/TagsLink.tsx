225158400d0ca9a33a6235dc8a0d4b5c402c7355
beyoko <blogbetest@protonmail.com>
2023-05-28 16:00:28 +1000

    day

--- src/components/TagsLink.tsx
+++ src/components/TagsLink.tsx
@@ -8,7 +8,7 @@ export default function TagsLink({ tags }: Props) {
   return tags.map(tag => {
     return (
       <a
-        className="p-2 text-lg border-4 border-gray-50 dark:border-gray-950 bg-gray-50 dark:bg-gray-950 hover:bg-gray-950 hover:text-gray-50 hover:dark:bg-gray-50 hover:dark:text-gray-950 rounded-lg transition-all"
+        className="text-lg border-4 border-gray-50 dark:border-gray-950 bg-gray-50 dark:bg-gray-950 hover:bg-gray-950 hover:text-gray-50 hover:dark:bg-gray-50 hover:dark:text-gray-950 rounded-lg transition-all"
         key={tag}
         href={`/categories/${tag.toLowerCase()}`}
       >
